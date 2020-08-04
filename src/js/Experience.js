bae13d59d957822d29cd4fcb363a6b5af2528199
jsonavalos <jsonavalos>
2020-08-03 21:58:22 -0700

    Added experience elements

--- src/js/Experience.js
+++ src/js/Experience.js
@@ -2,7 +2,8 @@ import React from 'react';
 import OLC from './OLCFacilitator.png'
 import INTERN from './SummerSupervisor.png'
 import './CSSfiles/HomeCSS.css'
-import DESK from './office.jpg'
+import CASPTEAM from './imagesSkills/supervisor.png'
+
 function Experience() {
     return (
         <div id="experience">
@@ -11,7 +12,7 @@ function Experience() {
             <section className="hero is-info is-small is-bold">
                 <div className="hero-body has-background-grey">
                     <div className="container has-text-left">
-                        <h1 className="title"> Work Experience </h1>
+                        <h1 className="title is-family-code has-text-left"> Work Experience </h1>
                     </div>
                 </div>
             </section>
@@ -22,11 +23,57 @@ function Experience() {
                     <div className="column is-8 is-offset-2">
                         {/* START ARTICLE */}
 
+                        <div className="card article">
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-content has-text-centered">
+                                        <p className="title article-title is-family-code">Program Assistant</p>
+                                        <div className="tags has-addons level-item">
+                                            <span className="tag is-rounded is-info">Chancellor's Associates Scholars Program</span>
+                                            <span className="tag is-rounded">June 15, 2020 - August 7, 2020</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <figure >
+                                    <img src={CASPTEAM} className="image is-1x1 is-centered"  alt="OLC"/>
+                                </figure>
+                                <br></br>
+                           <div className="columns is-gapless is-centered">
+                          
+                                <div className="card">
+
+                                    <header className="card-header has-background-success ">
+                                        <p className="card-header-title is-centered is-family-code">
+                                            DUTIES AND RESPONSABILITIES
+                                        </p>
+                                    </header>
+                                    <div className="card-content has-text-left is-family-code">
+                                        <div className="content is-8">
+                                            <ul>
+                                                <li>Provide general administrative support to the CASP office and its staff</li>
+                                                <li>General clerical duties</li>
+                                                <li>Checking in students</li>
+                                                <li>Helping create and update CASP publications</li>
+                                                <li>Light office clean-up and ordering</li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <br></br>
+                                </div>
+                               
+            
+                            </div>
+                            </div>
+                        </div>
+                   <br></br>
+
+
                         <div className="card article ">
                             <div className="card-content">
                                 <div className="media">
                                     <div className="media-content has-text-centered">
-                                        <p className="title article-title">OLC Facilitator</p>
+                                        <p className="title article-title is-family-code">Learning Community Facilitator</p>
 
                                         <div className="tags has-addons level-item">
                                             <span className="tag is-rounded is-info">Chancellor's Associates Scholars Program</span>
@@ -44,11 +91,11 @@ function Experience() {
                                    <div className="column">
                                     <div className="card">
                                         <header className="card-header has-background-success ">
-                                            <p className="card-header-title is-centered">
+                                            <p className="card-header-title is-centered is-family-code">
                                                 DUTIES AND RESPONSABILITIES
                                             </p>
                                         </header>
-                                        <div className="card-content has-text-left">
+                                        <div className="card-content has-text-left is-family-code">
                                             <div className="content is-8">
                                                 <ul>
                                                     <li>Prepare and organize program supplies and materials.</li>
@@ -65,6 +112,10 @@ function Experience() {
                             </div>
                             </div>
                         </div>
+
+
+
+                        
                         <br/>
                         {/* END ARTICLE */}
                         {/* Peer Leader */}
@@ -72,7 +123,7 @@ function Experience() {
                             <div className="card-content">
                                 <div className="media">
                                     <div className="media-content has-text-centered">
-                                        <p className="title article-title">Peer Leader</p>
+                                        <p className="title article-title is-family-code">Peer Leader</p>
                                         <div className="tags has-addons level-item">
                                             <span className="tag is-rounded is-info">Chancellor's Associates Scholars Program</span>
                                             <span className="tag is-rounded">September 20, 2019 - Present</span>
@@ -83,11 +134,11 @@ function Experience() {
           <div className="column is-gapless">
                                 <div className="card ">
                                     <header className="card-header has-background-success ">
-                                        <p className="card-header-title is-centered">
+                                        <p className="card-header-title is-centered is-family-code">
                                             DUTIES AND RESPONSABILITIES
                                         </p>
                                     </header>
-                                    <div className="card-content has-text-left">
+                                    <div className="card-content has-text-left is-family-code">
                                         <div className="content is-8">
                                             <ul>
                                                 <li>Work with a caseload of 8-10 Chancellor’s Associates Scholars (CASP) students by providing support and counseling related to academic, personal, and sociocultural issues affecting their college transition. </li>
@@ -122,63 +173,11 @@ function Experience() {
                         </div>
                         <br/>
                         {/* Peer Leader End */}
+
                         {/* Peer Leader */}
-                        <div className="card article">
-                            <div className="card-content">
 
 
 
-
-                                <div className="media">
-                                    <div className="media-content has-text-centered">
-                                        <p className="title article-title">Office Coordinator</p>
-                                        <div className="tags has-addons level-item">
-                                            <span className="tag is-rounded is-info">Chancellor's Associates Scholars Program</span>
-                                            <span className="tag is-rounded">January 20, 2020 - March 11, 2020</span>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-
-                           <div className="columns is-gapless">
-                                <div className="card">
-
-                                    <header className="card-header has-background-success ">
-                                        <p className="card-header-title is-centered">
-                                            DUTIES AND RESPONSABILITIES
-                                        </p>
-                                    </header>
-                                    <div className="card-content has-text-left">
-                                        <div className="content is-8">
-                                            <ul>
-                                                <li>Provide general administrative support to the CASP office and its staff</li>
-                                                <li>General clerical dutie</li>
-                                                <li>Checking in students</li>
-                                                <li>Helping create and update CASP publications</li>
-                                                <li>Light office clean-up and ordering</li>
-
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <figure >
-                                    <img src={DESK} width={200} height={100}  alt="OLC"/>
-                                </figure>
-
-
-
-
-                            </div>
-
-
-
-
-
-                            </div>
-                        </div>
                         <br/>
                         {/* Peer Leader End */}
                         {/* Summer Supervisor */}
@@ -186,7 +185,7 @@ function Experience() {
                             <div className="card-content">
                                 <div className="media">
                                     <div className="media-content has-text-centered">
-                                        <p className="title article-title">Summer Intern Supervisor</p>
+                                        <p className="title article-title is-family-code">Summer Intern Supervisor</p>
                                         <div className="tags has-addons level-item">
                                             <span className="tag is-rounded is-info">Chancellor's Associates Scholars Program</span>
                                             <span className="tag is-rounded">June 8, 2019 - July 26, 2019</span>
@@ -196,11 +195,11 @@ function Experience() {
 
                                 <div className="card">
                                     <header className="card-header has-background-success ">
-                                        <p className="card-header-title is-8 is-centered">
+                                        <p className="card-header-title is-8 is-centered is-family-code">
                                             DUTIES AND RESPONSABILITIES
                                         </p>
                                     </header>
-                                    <div className="card-content has-text-left">
+                                    <div className="card-content has-text-left is-family-code">
                                         <div className="content is-9">
                                             <ul>
                                                 <li>Prepare and organize program supplies and materials.</li>
