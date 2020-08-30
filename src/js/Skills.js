5c5efe7126ec2c2eb410fb39609278a8a7e0bfac
jsonavalos <jsonavalos>
2020-08-29 22:31:59 -0700

    Added URls

--- src/js/Skills.js
+++ src/js/Skills.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
 import JAVA from './java.png'
-import PYTHON from './imagesSkills/python.png'
-import DJANGO from './Django.png'
-import CPLUSPLUS from './imagesSkills/cplusplus.png'
 import HTML from './imagesSkills/html.png'
-import JAVASCRIPT from './imagesSkills/javascript.png'
 import R from './imagesSkills/R.png'
-import CSS from './imagesSkills/css.png'
 import ASSEMBLY from './imagesSkills/assembly.png'
 import C from './imagesSkills/C.png'
 import TYPESCRIPT from './imagesSkills/typescript.png'
-import VISUAL from './imagesSkills/visual.png'
 import PYCHARM from './imagesSkills/pycharm.png'
-import WEBSTORM from './imagesSkills/webstorm.png'
 import MATHLAB from './imagesSkills/mathlab.png'
 import LINUX from './imagesSkills/linux.png'
 import GITHUB from './imagesSkills/github.png'
@@ -44,7 +37,7 @@ function Skills() {
                             <div className="column is-3">
                                 <div className="level">
                                     <div className="level-item">
-                                        <figure className="image is-128x128"><img className="is-rounded" src={CPLUSPLUS} alt="" /></figure>
+                                        <img src="https://jjwhbq.bn.files.1drv.com/y4moQeqmySjaW5K64Mg5HsJaIupY8yd9zJfgdL5lB9L3b6hdryryIF8LYTs6SDsR1bXx9ynRoxv2yEUwf3bmwDpqXJ0_p5TTJw6OoXsgtqyROwXu12HPdsKytkA1hUvn7IPsLlNpS3dguLFELpDBCRCGAp9wj8n5DaRoRH6Uh9zZS3GWS4PGavZkprQhVndgBgSwjTHzldm6H9ykrOHWeN7GQ?" width="128" height="128" alt="C++" />
                                     </div>
                                 </div>
                                 <h5 className="title is-5">C++</h5>
@@ -54,7 +47,7 @@ function Skills() {
                             <div className="column is-3">
                                 <div className="level">
                                     <div className="level-item">
-                                        <figure className="image is-128x128"><img className="is-rounded" src={PYTHON} alt="" /></figure>
+                                    <img src="https://jjvrha.bn.files.1drv.com/y4mO41aWThQCHkxWz4O4KZjjrbrVdyjTWPTCvoh8BlC7ZclWfbV39451Wr3bGKSiC4jP0EoelyZxTiQGHZnHg1BnxLY5BTw6Av5BGzQoksrBARx628sfzcHboFm-Rma5-BaoX8Vb6B-ww6z-6JQxH7OWKzxqK_Emcf7TO_U5JSAoJpt76ZS7bcPSZ7-2uWkNgNMkkvpGNwbKu6EUwiNvBC2iQ?" width="128" height="128" alt="Python" />
                                     </div>
                                 </div>
                                 <h5 className="title is-5">Python</h5>
@@ -86,7 +79,7 @@ function Skills() {
                             <div className="column is-3">
                                 <div className="level">
                                     <div className="level-item">
-                                        <figure className="image is-96x96"><img className="" src={JAVASCRIPT} alt="" /></figure>
+                                    <img src="https://8sd6yg.bn.files.1drv.com/y4mezklGaKlWO-YcAna1Bnn-pwK64cHF3yxJI1TF6RLpful3N-ix5leVVcFc63tAoRy95Vik2UfVartYXQ7rAR8aG_Bos2xAzQPbOfkA1V21Bpn6ACdrbZ9hsqFBDRZ-H3YclxejzL3vGopScrJOcznhhryI3nmIzMUpRVWghZfbI-j3o6hYQbAlSsYybeB3iATHw3KQ-z1cvVzfS9y8IPC6g?" width="96" height="96" alt="Javascript" />
                                     </div>
                                 </div>
                                 <h5 className="title is-5">Javascript</h5>
@@ -116,7 +109,7 @@ function Skills() {
                             <div className="column is-3">
                                 <div className="level">
                                     <div className="level-item">
-                                        <figure className="image is-96x96"><img className="" src={CSS} alt="" /></figure>
+                                    <img src="https://jjxqdw.bn.files.1drv.com/y4m4Sc7duhq3Ufn6Y4H6HpJRK5PsjoTEZg1Pjn3_o6BB9jcGAH1-swHgz551e1ZeizXMOYIb-O_PvlESjBiw-6_qc3aDT1SAzebDig_tTIxJajG9RIo12W7ARPKSuY3KB5DiHsuyCUQuJsECqW_MYfbxxyoBMW879RFacrjHV4DUYfb6hwGOtiXw9VFqf77ylzBQQy1ljYkz436izEQx9of0g?" width="96" height="96" alt="CSS" />
                                     </div>
                                 </div>
                                 <h5 className="title is-5">CSS</h5>
@@ -148,7 +141,7 @@ function Skills() {
                             <div className="column is-3">
                                 <div className="level">
                                     <div className="level-item">
-                                        <figure className="image is-128x128 "><img src={DJANGO} alt="" /></figure>
+                                    <img src="https://jjuuhw.bn.files.1drv.com/y4m3GldWnu2j4Xqw7jJ67n0u3BYG5AMgNdY5flbl4AUoI8SKozMUggIbw1NK0uEFzp-VomAei9_i5Id-bjeEhMS9k9fswEX32bwZMoX_w6KkjzcYe4MGvAjoXLrrwsZYIaHBDbAt2DjtmFK7UZHADQmDwRSehViMK4LC5ZGBd1rRIZFNspB4ya_cPGqXDOmBAUE2QNEtXt2GjC6JiBLQcqqFw?" width="128" height="128" alt="Django"/>
                                     </div>
                                 </div>
                                 <h5 className="title is-5">Django</h5>
@@ -167,7 +160,7 @@ function Skills() {
                             <div className="column is-3">
                                 <div className="level">
                                     <div className="level-item">
-                                        <figure className="image is-128x128"><img className="is-rounded" src={VISUAL} alt="" /></figure>
+                                    <img src="https://jjwajg.bn.files.1drv.com/y4mgqDjr2D-qpdLaAwXKhU94Ier5z2_WwWVXAV6j_hjy1PjkalTU0sp0hfqNCs4L4kapCWWlDDiDHuxzHbROjwIADErVt-vzfWPC_pXjEuURFMf6ADANSbXfyKKE4YXKgcOY9u8gr-Nj_Xdc3WBH47V2zluMr3CD9HN6UXDsMkl8nJKw0Yar9yidGYK9Ru3HPSx3gZ-qOtwkVCduL4YsEOMlg?" width="110" height="110" alt="Visual Code" />
                                     </div>
                                 </div>
                                 <h5 className="title is-5">Visual Studio Code</h5>
@@ -185,7 +178,7 @@ function Skills() {
                             <div className="column is-3">
                                 <div className="level">
                                     <div className="level-item">
-                                        <figure className="image is-128x128"><img className="is-rounded" src={WEBSTORM} alt="" /></figure>
+                                    <img src="https://jju1mw.bn.files.1drv.com/y4mzmqcy9jZdk_ppD1VPfmiP8096-P_hGaarJ2hitj8vZlXaHMzTYC_pvoUFesiupNlkRveNwMKAndSfqyF7w1mTcPNHULzrGzZr6eJ85alkxTY1HXrs0FyR0JcNFnIpwkD2A3ATs1Jet0CIcJN_nLVbPCfD0RiRk4DEkWZt0Yis7wAL2z7s_GZCf1ai0cZFCwMEawWllbkcgN_hppCw6v00w?" width="128" height="128" alt="Webstorm" />
                                     </div>
                                 </div>
                                 <h5 className="title is-5">Webstorm</h5>
